47cf093971b5611b71e6fb3ff073c045f29330d5
Aishwary Dewangan <aishwary.dewangan@gmail.com>
2018-08-24 22:42:43 +0530

    Error Handling

--- rename.cpp
+++ rename.cpp
@@ -3,15 +3,30 @@
 
 using namespace std;
 
-void newName(const char *oldFileName, const char *newFileName) {
-	int status = rename(oldFileName, newFileName);
-	if(status == 0)
-		cout << "\nFile renamed successfully";
-	else
-		cout << "\nError: Cannot rename file. Please check for permissions!";
+bool isFileExists(const char *name) {
+	ifstream file(name);
+	bool check = file.good();
+	file.close();
+	return check;
+}
+
+void renameFile(const char *oldFileName, const char *newFileName) {
+	if(isFileExists(oldFileName)) {
+		if(!isFileExists(newFileName)) {	
+			int status = rename(oldFileName, newFileName);
+			if(status == 0)
+				cout << "\nFile renamed successfully";
+			else
+				cout << "\nError: Cannot rename file. Please check for permissions!";
+		} else {
+			cout << "\nError: Destination File already exists. Please check name!";
+		}
+	} else {
+		cout << "\nError: Source File not found. Please check name!";
+	}
 }
 
 int main() {
-	newName("hello.txt", "hey.mp4");
+	renameFile("hello.txt", "hey.mp4");
 	return 0;
 }
